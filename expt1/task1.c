14e228027ba68dc8a67812b1719ec8b8d8712bf0
AnsariUzma006 <uzmacom@eng.rizvi.edu.in>
2024-10-16 10:25:08 +0530

    Create task1.c

---
+++ expt1/task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main () {
+  printf("Name\t:Ansari Uzma\n");
+  printf("UIN\t: 241S064\n");
+  printf ("Age\t: \t18\n");
+  printf ("Class\t: \tFE\n");
+  printf ("Div\t: \tE\n");
+
+return 0;
+}
